d61c51f51c6688db8dd9ad7b070b4909cfaa9618
yunuskazan81 <yunuskazan1987@gmail.com>
2019-06-28 17:42:26 -0400

    brand new commit

---
+++ src/com/Study3/newbranches.java
@@ -0,0 +1,11 @@
+package com.Study3;
+
+public class newbranches {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("i removed and create new local branch");
+	}
+
+}
