005c230df2296514d2a161a44570f85ef8f6efff
yunuskazan81 <48811543+yunuskazan81@users.noreply.github.com>
2019-06-28 19:06:11 -0400

    Update NewGitBraches.java

--- src/com/Study3/NewGitBraches.java
+++ src/com/Study3/NewGitBraches.java
@@ -1,2 +1,15 @@
 package com.Study3;
 
+public class NewGitBraches {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 5;
+		int b= 8;
+		int c= a+b;
+		int d= a+(c*b);
+		System.out.println("it is common branch "+ (a*b/c)*d);
+	}
+
+}
